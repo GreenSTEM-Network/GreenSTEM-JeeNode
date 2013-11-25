1b7ee3aa4caec7f04204cb140b809d18dc64c932
Rob Bender <rbender@gmail.com>
2013-11-25 17:44:00 -0500

    Minor cleanup
    The Arduino IDE requires sketches to reside in a directory with the
    same name as the sketch, so I moved JeeNode_Send.ino into a
    JeeNode_Send folder.
    
    Also did some minor code cleanup, including fixing indentation, adding
    comments and extra constants.

--- JeeNode_Send/JeeNode_Send.ino
+++ JeeNode_Send/JeeNode_Send.ino
@@ -9,6 +9,13 @@ ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 //Define RF Node ID and Channel
 #define RF_NODEID 1
 #define RF_CHANNEL 100
+#define TIME_BETWEEN_READS 5000 // in milliseconds
+
+//Define physical analog pins to read from
+#define SOIL1_PIN 0
+#define SOIL2_PIN 1
+#define SOIL3_PIN 2
+#define TEMPERATURE_PIN 3
 
 typedef struct {
   int nodeid;
@@ -17,6 +24,7 @@ typedef struct {
   float soil_3;
   float temp;
 } Payload;
+
 Payload data;
 
 void setup () {
@@ -25,20 +33,28 @@ void setup () {
 }
 
 void loop () {
-    while (!rf12_canSend())
+
+  //Wait until JeeNode is ready to send data
+  while (!rf12_canSend()) {    
     rf12_recvDone();
-      data.soil_1 = analogRead(0);
-      data.soil_2 = analogRead(1);
-      data.soil_3 = analogRead(2);
-      data.temp = analogRead(3);
+  }
   
+  //Read data from sensors
+  data.soil_1 = analogRead(SOIL1_PIN);
+  data.soil_2 = analogRead(SOIL2_PIN);
+  data.soil_3 = analogRead(SOIL3_PIN);
+  data.temp = analogRead(TEMPERATURE_PIN);
   
   // send packet out
+  send_data();
+  
+  // go to sleep for approx 5 seconds
+  Sleepy::loseSomeTime(TIME_BETWEEN_READS);
+}
+
+void send_data() {
   rf12_sleep(RF12_WAKEUP);
   rf12_sendNow(0, &data, sizeof data);
   rf12_sendWait(2);
   rf12_sleep(RF12_SLEEP);
-  
-  // go to sleep for approx 5 seconds
-  Sleepy::loseSomeTime(5000);
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ JeeNode
 =======
 
 Arduino-compatible code for running the sensors
+
+This code requires the JeeLib library from https://github.com/jcw/jeelib
+
